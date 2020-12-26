b53a30632a7f4020986077ef2f5022ade11376cb
Mandi Awale <mawale@ap.equinix.com>
2020-12-26 18:28:27 +1100

    added dummy data to history page

--- src/components/menu/history/History.tsx
+++ src/components/menu/history/History.tsx
@@ -1,11 +1,61 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+	List,
+	ListItem,
+	Divider,
+	ListItemText,
+	ListItemAvatar,
+	Avatar,
+	Typography,
+	Paper,
+} from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+	root: {
+		width: '100%',
+	},
+	inline: {
+		display: 'inline',
+	},
+}));
 
 const History = () => {
-    return (
-        <div>
-            <h1>History: Activities log</h1>
-        </div>
-    )
-}
+	const classes = useStyles();
+	return (
+		<div>
+			<Paper>
+				<List className={classes.root}>
+					<ListItem>
+						<ListItemAvatar>
+							<Avatar alt='Remy Sharp' src='/static/images/avatar/1.jpg' />
+						</ListItemAvatar>
+						<ListItemText
+							primary='You lent to Katy, $300 for trip to Canberra on 10/12/20.'
+						/>
+					</ListItem>
+					<Divider variant='inset' component='li' />
+                    <ListItem>
+						<ListItemAvatar>
+							<Avatar alt='Remy Sharp' src='/static/images/avatar/1.jpg' />
+						</ListItemAvatar>
+						<ListItemText
+							primary='You borrowed from Dad, $10,000 for startup capital on 04/11/20.'
+						/>
+					</ListItem>
+					<Divider variant='inset' component='li' />
+                    <ListItem>
+						<ListItemAvatar>
+							<Avatar alt='Remy Sharp' src='/static/images/avatar/1.jpg' />
+						</ListItemAvatar>
+						<ListItemText
+							primary='You lent Katy $300 for trip to Canberra on 10/12/20.'
+						/>
+					</ListItem>
+				</List>
+			</Paper>
+		</div>
+	);
+};
 
-export default History
+export default History;
