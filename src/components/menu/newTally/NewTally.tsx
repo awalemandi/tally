ffc80d0d3daa6d901a3b6b7f1a3ed3b2bb8f5bcb
awalemandi <mandi.awale@gmail.com>
2021-02-05 13:32:21 +1100

    changed new tally form to flex from grid

--- src/components/menu/newTally/NewTally.tsx
+++ src/components/menu/newTally/NewTally.tsx
@@ -12,15 +12,45 @@ import Category from './Category';
 
 const useStyles = makeStyles(theme => ({
 	form: {
+
 	},
 	root: {
-		width: '100%',
-		marginTop: '1rem',
-		padding: 0,
+		width: '70%',
+		height: 'auto',
+		marginTop: theme.spacing(4),
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'center',
+		alignItems: 'center',
+		[theme.breakpoints.down('lg')]: {
+			width: '90%'
+		},
+		[theme.breakpoints.down('sm')]: {
+			width: '100%'
+		},
+	},
+	inputWrapper: {
+		textAlign: 'left',
+		width: '25%',
+		marginBottom: theme.spacing(1),
+		padding: theme.spacing(1),
+		[theme.breakpoints.down('lg')]: {
+			width: '35%'
+		},
+		[theme.breakpoints.down('md')]: {
+			width: '30%'
+		},
+		[theme.breakpoints.down('sm')]: {
+			width: '40%'
+		},
+		[theme.breakpoints.down('xs')]: {
+			width: '70%'
+		}
 	},
 	actionButton: {
-		minWidth: '200px',
-		minHeight: '70px',
+		marginTop: theme.spacing(2),
+		minWidth: 200,
+		minHeight: 70,
 	},
 	icon: {
 		marginRight: theme.spacing(1),
@@ -32,19 +62,47 @@ const NewTally = () => {
 	return (
 		<form noValidate autoComplete='off' className={classes.form}>
 			<Typography variant='h3'>Add a new Tally</Typography>
-			<Grid
+			<div className={classes.root}>
+				<div className={classes.inputWrapper}>
+					<TransactionType />
+				</div>
+				<div className={classes.inputWrapper}>
+					<Amount />
+				</div>
+				<div className={classes.inputWrapper}>
+					<Reason />
+				</div>
+				<div className={classes.inputWrapper}>
+					<Party />
+				</div>
+				<div className={classes.inputWrapper}>
+					<Date />
+				</div>
+				<div className={classes.inputWrapper}>
+					<Category />
+				</div>
+				<Fab
+					variant='extended'
+					color='primary'
+					className={classes.actionButton}
+				>
+					<TiPlusOutline className={classes.icon} />
+					Add Tally
+				</Fab>
+			</div>
+			{/* <Grid
 				className={classes.root}
 				container
 				spacing={5}
 				direction='column'
-				justify='flex-start'
-				alignItems='stretch'
+				justify='center'
+				alignItems='center'
 			>
-				<Grid item xs={10} sm={8} md={5} lg={3}>
+				<Grid item xs={10} sm={8} md={5} >
 					<TransactionType />
 				</Grid>
 				<Hidden mdDown>
-					<Grid item md={6} lg={9}></Grid>
+					<Grid item md={6} ></Grid>
 				</Hidden>
 				<Grid item xs={10} sm={8} md={5} lg={3}>
 					<Amount />
@@ -89,7 +147,7 @@ const NewTally = () => {
 				<Hidden mdDown>
 					<Grid item md={6} lg={9}></Grid>
 				</Hidden>
-			</Grid>
+			</Grid> */}
 		</form>
 	);
 };
