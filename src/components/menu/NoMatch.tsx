116f0a17a88a5983c632c69c958dd2c9cc5890ac
Mandi Awale <mawale@ap.equinix.com>
2021-02-06 05:31:03 +1100

    built noMatch page

--- src/components/menu/NoMatch.tsx
+++ src/components/menu/NoMatch.tsx
@@ -1,16 +1,33 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
+import { Typography, Button } from '@material-ui/core';
+import { RiArrowGoBackLine } from 'react-icons/ri';
 
 const NoMatch = () => {
 	let location = useLocation();
 
 	return (
 		<div>
-			<h2>Oops! Page not found.</h2>
-			<h3>
+			<Typography variant='h2' color='primary' style={{marginTop: 5}}>
+				Oops! Page not found.
+			</Typography>
+			<Typography variant='h4' color='inherit' style={{marginTop: 5}}>
 				<code>{location.pathname}</code>
-			</h3>
-			<p>The page you're looking for doesn't seem to exist.</p>
+			</Typography>
+			<Typography variant='subtitle1' style={{marginTop: 5}}>
+				The page you're looking for doesn't seem to exist.
+			</Typography>
+			<Link to='/home/dashboard' style={{textDecoration: 'none'}}>
+			<Button
+				color='primary'
+				variant='outlined'
+				size='large'
+				startIcon={<RiArrowGoBackLine />}
+				style={{marginTop: 10}}
+			>
+				Go Back
+			</Button>
+			</Link>
 		</div>
 	);
 };
